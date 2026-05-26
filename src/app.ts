d6d7e8c7cc9f5aded783c7db4bb263eef5763092
darshana09235 <darshana09235@gmail.com>
2026-05-26 22:46:41 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(
 
 app.use(cors({
   origin: true,
-  credentials: true,
+  credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -41,7 +41,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: true,
+sameSite: "none",
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   },
